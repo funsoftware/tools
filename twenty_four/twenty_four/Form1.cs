1018b7f4625daa172add38dc89bf38aaa963269f
JoeLiu <liujian.xa@gmail.com>
2017-03-18 08:58:10 +0800

    minor

--- twenty_four/twenty_four/Form1.cs
+++ twenty_four/twenty_four/Form1.cs
@@ -22,84 +22,62 @@ namespace twenty_four {
       return calc(nums, 24.00000f, ref expr, ref val, ref state) ? expr : "";
     }
     private bool calc(int[] nums, double result, ref string expr, ref double val, ref int state) {
-      if (nums.Length == 0) {
-        return val == result;
-      }
+      if (nums.Length == 0)  return val == result;
+      // state=-1 ----  empty(start)
+      // state=0  ----  single number, 
+      // state=1  ----  + or -, 
+      // state=2  ----  * or /
+      String newExpr;
+      double newVal;
+      int newState;
       for (int i = 0; i < nums.Length; i++) {
         int num = nums[i];
         List<int> temp = new List<int>(nums);
         temp.RemoveAt(i);
         int[] newNums = temp.ToArray();
-        String newExpr = expr;
-        double newVal = val;
-        int newState = state;
-        if (state == -1) { // Not start
+        if (state == -1) { 
           newExpr = num.ToString();
           newVal = num;
           newState = 0;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
-        } else {
-
-          newExpr = num + "+" + expr;
-          newVal = num + val;
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) goto success;
+        } else { 
+          // 1. expr + num
+          newExpr = expr + "+" + num;
+          newVal = val + num;
           newState = 1;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
-
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) goto success;
+          // 2. expr - num
           newExpr = expr + "-" + num;
           newVal = val - num;
           newState = 1;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
-
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) goto success;
+          // 3. num - expr
           newExpr = num + "-" + (state == 1 ? "(" + expr + ")" : expr);
           newVal = num - val;
           newState = 1;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
-
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) goto success;
+          // 4. expr * num
           newExpr = (state == 1? "(" + expr + ")" : expr) + "*" + num;
           newVal = val * num;
           newState = 2;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
-
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) goto success;
+          // 5. expr / num
           newExpr = (state == 1 ? "(" + expr + ")" : expr) + "/" + num;
           newVal = val / num;
           newState = 2;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
-
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState))  goto success;
+          // 6. num / expr
           newExpr = num + "/" + (state >= 1 ? "(" + expr + ")" : expr);
           newVal = num / val;
           newState = 2;
-          if (calc(newNums, result, ref newExpr, ref newVal, ref newState)) {
-            expr = newExpr;
-            val = newVal;
-            return true;
-          }
+          if (calc(newNums, result, ref newExpr, ref newVal, ref newState))  goto success;
         }
       }
       return false;
+success:
+      expr = newExpr;
+      val = newVal;
+      return true;
     }
 
     private void textBox2_TextChanged(object sender, EventArgs e) {
